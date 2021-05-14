5489eb305dc9c8ca9f732861072d7a1cf7f1eeea
celestet <celestetan23@gmail.com>
2021-05-14 11:15:45 -0700

    Add files via upload

---
+++ productcalc/cmake-build-debug/CMakeFiles/productcalc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/productcalc.dir/main.cpp.o"
+  "productcalc"
+  "productcalc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/productcalc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
