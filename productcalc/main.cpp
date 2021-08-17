4ca1d302a596453680d3d14d44816ae2d43d66a2
celestet <celestetan23@gmail.com>
2021-08-17 11:00:49 -0700

    edited

--- productcalc/main.cpp
+++ productcalc/main.cpp
@@ -6,32 +6,32 @@ int main() {
 
     int number, exponent, number2, exponent2, number3, e3;
 
-    cout << "the variable is x. This program gets two polynomials and gets the derivative of the product" << endl;
+    cout << "The variable is x. This program gets two polynomials and gets the derivative of the product" << endl;
 
-    cout << "enter a number\n";
+    cout << "Enter a number\n";
     cin >> number;
-    cout << "enter a exponent\n";
+    cout << "Enter a exponent\n";
     cin >> exponent;
 
-    cout << "you have entered this polynomial\n";
+    cout << "You have entered this polynomial\n";
     cout << number << 'x' << "^" << exponent;
 
-    cout << "\nenter anither number\n";
+    cout << "\nEnter another number\n";
     cin >> number2;
-    cout << "enter anouther exponent\n";
+    cout << "Enter another exponent\n";
     cin >> exponent2;
 
-    cout << "\nyou have entered this polynomial\n";
+    cout << "\nYou have entered this polynomial\n";
     cout << number2 << 'x' << "^" << exponent2 << endl;
 
     number3 = number*number2;
     e3 = exponent+exponent2;
 
-    cout << "the two polynomials multiplied together is " << number3 << 'x' << "^" << e3 << endl;
+    cout << "The two polynomials multiplied together is " << number3 << 'x' << "^" << e3 << endl;
 
 
 
-    cout << "The derivate of the product is " << number3*e3 << 'x' << "^" << e3 -1;
+    cout << "The derivative of the product is " << number3*e3 << 'x' << "^" << e3 -1;
 }
 
 //sample output
